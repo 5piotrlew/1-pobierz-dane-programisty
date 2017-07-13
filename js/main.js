33ebcdb93a64b78ccbba875cc49cd2fc44d8a285
Piotr Lewinski <piotrlew5@wp.pl>
2017-07-13 23:17:14 +0200

    finish

--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
 'use strict';
 
 $(function () {
-    console.log('hello');
+
+    $("body").append('<div id="dane-programisty"></div>');
 
     $('#pobierz-dane').click(function () {
+
         $.getJSON('http://echo.jsontest.com/imie/Piotr/nazwisko/Lewinski/zawod/Programista/firma/Akademia108', function (data) {
-            $("#dane-programisty").append('<p>Imię użytkownika: ' + data.imie + '</p>');
-            $("#dane-programisty").append('<p>Nazwisko użytkownika: ' + data.nazwisko + '</p>');
-            $("#dane-programisty").append('<p>Zawód użytkownika: ' + data.zawod + '</p>');            
-            $("#dane-programisty").append('<p>Firma użytkownika: ' + data.firma + '</p>');
+
+        $("#dane-programisty").append('<p class="text-danger">Imię użytkownika: ' + data.imie + '</p>');
+        $("#dane-programisty").append('<p class="text-danger">Nazwisko użytkownika: ' + data.nazwisko + '</p>');
+        $("#dane-programisty").append('<p class="text-danger">Zawód użytkownika: ' + data.zawod + '</p>');
+        $("#dane-programisty").append('<p class="text-danger">Firma użytkownika: ' + data.firma + '</p>');
+            
+        $('p').addClass("bg-warning");
         });
     });
+
+
 
 });
